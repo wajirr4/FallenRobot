5898c74e670308dec14a277474fd29ee4c70b9aa
wajirr4 <109944533+wajirr4@users.noreply.github.com>
2022-08-21 02:22:21 +0600

    Create config.py

---
+++ FallenRobot/config.py
@@ -0,0 +1,65 @@
+class Config(object):
+    LOGGER = True
+    # REQUIRED
+    # Login to https://my.telegram.org and fill in these slots with the details given by it
+
+    API_ID = 16596628  # integer value, dont use ""
+    API_HASH = "421764a823ee2dff786d413aea09959f"
+    TOKEN = "5416296821:AAFfHmVs76xg2YUJ14f5gAH1eSVFZ-tPl40"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 5136746907  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "wajirrr"
+    SUPPORT_CHAT = "xteamBDChat"  # Your own group for support, do not add the @
+    JOIN_LOGGER = (
+        -1001761930779
+    )  # Prints any new group the bot is added to, prints just the name and ID.
+    EVENT_LOGS = (
+        -1001761930779
+    )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
+
+    # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = "postgres://infodot:4455Hasan@137.184.79.64:5432/tgbot"  # needed for any database modules
+    LOAD = []
+    NO_LOAD = ["rss", "cleaner", "connection", "math"]
+    WEBHOOK = False
+    INFOPIC = True
+    URL = None
+    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
+
+    # OPTIONAL
+    ##List of id's -  (not usernames) for users which have sudo access to the bot.
+    DRAGONS = get_user_list("elevated_users.json", "sudos")
+    ##List of id's - (not usernames) for developers who will have the same perms as the owner
+    DEV_USERS = get_user_list("elevated_users.json", "devs")
+    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    DEMONS = get_user_list("elevated_users.json", "supports")
+    # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    TIGERS = get_user_list("elevated_users.json", "tigers")
+    WOLVES = get_user_list("elevated_users.json", "whitelists")
+    DONATION_LINK = None  # EG, paypal
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.
+    STRICT_GBAN = True
+    WORKERS = (
+        8  # Number of subthreads to use. Set as number of threads your processor uses
+    )
+    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
+    CASH_API_KEY = (
+        "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
+    )
+    TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
+    WALL_API = (
+        "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
+    )
+    BL_CHATS = []  # List of groups that you want blacklisted.
+    SPAMMERS = None
+
+
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True
